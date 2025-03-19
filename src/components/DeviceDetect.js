5c16e0e2d92a51e78589f4b72cc6518569a38e7a
minthwayhtut <minthwayhtut568@gmail.com>
2025-03-19 14:26:06 +0100

    Another Implementation

--- src/components/DeviceDetect.js
+++ src/components/DeviceDetect.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React,{ useEffect, useState } from 'react';
 import { isMobile, isDesktop } from 'react-device-detect';
 
+
 function DeviceBasedComponent() {
+  
+  const [orientation, setOrientation] = useState({alpha: 0, beta: 0, gamma: 0});
+
+useEffect(() => {
+  if (isMobile) {
+    const handleOrientation = (event) => {
+      setOrientation({
+        alpha: event.alpha,
+        beta: event.beta,
+        gamma: event.gamma,
+      });
+    };
+
+    window.addEventListener("deviceorientation", handleOrientation);
+    return () => window.removeEventListener("deviceorientation", handleOrientation);
+  }
+}, []);
+
+
+
   return (
     <div>
       {isMobile && (
         <div>
           <h2>Mobile-Only Feature</h2>
           <p>This feature is only visible on mobile devices!</p>
+          <p id="alpha">Alpha: {orientation.alpha} </p>
+         <p id="beta">Beta: {orientation.beta}</p>
+         <p id="gamma">Gamma: {orientation.gamma}</p>
         </div>
       )}
 
